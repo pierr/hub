258fe959fa4c77410dc837583079399c7753c749
Pierre <pierre.besson7@gmail.com>
2014-10-30 23:24:03 +0100

    Update the build.

--- gulpfile.js
+++ gulpfile.js
@@ -9,5 +9,17 @@ gulp.task('templates', function() {
     .pipe(jade({
       client: true
     }))
-    .pipe(gulp.dest('./build/'));
+    .pipe(gulp.dest('./lib/templates/'));
+});
+
+gulp.task('browserify', ['templates'] ,function() {
+  var browserify = require('browserify');
+  var source = require('vinyl-source-stream');
+  return browserify('./lib/index.js')
+    .bundle()
+    //Pass desired output filename to vinyl-source-stream
+    .pipe(source('hub.js'))
+    // Start piping stream to tasks!
+    .pipe(gulp.dest('./dist/'))
+    .pipe(gulp.dest('./example/'));
 });
